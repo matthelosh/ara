c27d625e62ec7ee332e28641f749ea4eaced7fd1
matthelosh <matthelosh@gmail.com>
2022-07-02 13:51:34 +0700

    Deletesome comments on web.php

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', [FrontController::class, 'index'])->name('index');
 // Route::get('/tes', [FrontController::class, 'index'])->name('tes');
