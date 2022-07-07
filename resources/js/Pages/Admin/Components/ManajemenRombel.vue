04f8a4dc2a56afedd50415c079402a0ff22dd90f
matthelosh <matthelosh@gmail.com>
2022-07-07 08:02:05 +0700

    Progress Admin Rombel Manajemen1

---
+++ resources/js/Pages/Admin/Components/ManajemenRombel.vue
@@ -0,0 +1,25 @@
+<template>
+	<v-container>
+		<v-row>
+			<v-col>
+				Tabel Anggota Rombel
+			</v-col>
+			<v-col>
+				Tabel Siswa
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+<script>
+	export default {
+		name: 'ManajemenRombel',
+		props: {rombel: Object},
+		data: () => ({
+			members: [],
+			nonmembers: [],
+			headerMembers: [],
+			headerNonMembers: [],
+			search:''
+		}),
+	}
+</script>
