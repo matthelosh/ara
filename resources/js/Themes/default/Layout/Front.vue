5b8bf3b0fe7be701ea35187d5423da22f07867c3
matthelosh <matthelosh@gmail.com>
2022-07-11 15:21:55 +0700

    Add Default Theme Folder

---
+++ resources/js/Themes/default/Layout/Front.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-app>
+        <v-container fill-height align-center justify-center>
+            <slot />
+        </v-container>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: 'Front',
+}
+</script>
---
+++ resources/js/Themes/default/Pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Selamat Datang</h1>
+    </div>
+</template>
+
+<script>
+import Front from '../Layout/Front'
+export default {
+    name: 'Index',
+    layout: Front
+}
+</script>
